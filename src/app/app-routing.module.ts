c999f534ae831fa4c7b1164d75a693430de2ed27
Gorakh Shinde <gorakhshankarshinde@gmail.com>
2025-04-18 10:36:04 +0200

    space removed

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,6 @@ import { RouterModule, Routes } from '@angular/router';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { AboutComponent } from './about/about.component';
 import { LoginComponent } from './login/login.component';
-
 import { HomeComponent } from './home/home.component';
 import { ShadowdomExampleComponent } from './shadowdom-example/shadowdom-example.component';
 import { MenucardComponent } from './menucard/menucard.component';  
